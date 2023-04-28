1ac3ea4ad5cfba5ca1d6e0826d184a5218612b85
Tommogo-coder <nelsontommogo9@gmail.com>
2023-04-28 08:29:47 +0300

    Java Inventory..... JDBC Updated

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ var sem2=3.55
 var sem3=3.99
 var Average=sem1+sem2+sem3
 console.log(Myname, MyAge, School, gender,College, sem1, sem2, sem3, Average)
+if(sem1>sem2)
+{
+    console.log("You dropped")
+}
+else if(sem2>sem1){
+    console.log("Improved")
+}
+else{
+console.log("didn't attend any lectures")
+}
