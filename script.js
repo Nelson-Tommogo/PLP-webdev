d4c4c893d1e1b818f1970a82807192cfcf7fb978
Tommogo-coder <nelsontommogo9@gmail.com>
2023-04-28 07:41:19 +0300

    PLP Web Dev Sessions

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let Myname="Nelson Tommogo";
-let MyAge=22;
+let Myname="Nelson Tommogo"
+let MyAge=22
 let School="KCA UNIVERSITY"
-const gender="Male";
-var College="PLP Africa";
-var sem1=3.46;
-var sem2=3.55;
-var sem3=3.99;
-var Average=sem1+sem2+sem3;
-console.log(Average);
+const gender="Male"
+var College="PLP Africa"
+var sem1=3.46
+var sem2=3.55
+var sem3=3.99
+var Average=sem1+sem2+sem3
+console.log(Myname)
